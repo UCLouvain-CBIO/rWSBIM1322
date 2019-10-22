750f87262244766560294fb9afa93c9623ff54b2
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-10-23 00:09:59 +0200

    add cptac data

---
+++ R/rWSBIM1322.R
@@ -0,0 +1,10 @@
+#' @importFrom utils packageVersion
+
+#' @title Package version
+#'
+#' @export
+#' @examples
+#' ## check the package version that is currently installed
+#' rWSBIM1322version()
+rWSBIM1322version <- function()
+    utils::packageVersion("rWSBIM1322")
