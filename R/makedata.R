dede08450d1e731b4aefa8fc2cc4129b47f5b720
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-10-31 17:39:42 +0100

    version 0.2.0 with make_data()

---
+++ R/makedata.R
@@ -0,0 +1,46 @@
+##' @title Generate data
+##' 
+##' @param noma `character(1)` that can be coerced into a numeric
+##' 
+##' @return A SummarizedExperimet
+##'
+##' @export
+##'
+##' @examples
+##' make_data("123")
+make_data <- function(noma) {
+    if (!is.character(noma))
+        stop("'noma' must be a character.")
+    stopifnot(length(noma) == 1)
+    
+    noma <- as.numeric(noma)
+    set.seed(noma)
+
+    stopifnot(suppressPackageStartupMessages(require("SummarizedExperiment")))
+    
+    ## aa <- readAAStringSet("./up.fasta.gz")
+    ## names(aa) <- sub("\\|.+$", "", sub("sp\\|", "", names(aa)))
+    aaf <- dir(system.file("extdata", package = "rWSBIM1322"),
+               full.names = TRUE, pattern = "aanames.rds")
+    aanames <- readRDS(aaf)
+
+    ## prepare expression data
+    data(cptac_se_prot, package = "rWSBIM1322")
+    x <- cptac_se_prot
+    metadata(x) <- list()
+
+    ## change row data
+    rownames(x) <- sample(aanames, nrow(x))
+    rowData(x) <- DataFrame(proteins = rownames(x),
+                            row.names = rownames(x))
+
+    ## change col data
+    colnames(x) <- paste0("S", 1:6)
+    colData(x) <- DataFrame(row.names = colnames(x))
+    x$group <- rep(c("CTRL", "DRUGA"), each = 3)
+    x$replicate <- rep(1:3, 2)
+    x <- x[, sample(6)]
+    x <- x[sample(nrow(x)), ]
+
+    return(x)
+}
