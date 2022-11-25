782af41b081c3b900bf3575fe2bc6e8f3619b323
Laurent Gatto <laurent.gatto@uclouvain.be>
2022-11-25 16:53:49 +0100

    update recap data

--- inst/scripts/recapSE.R
+++ inst/scripts/recapSE.R
@@ -1,4 +1,5 @@
 library(wsbimtools)
+library(SummarizedExperiment)
 
 ## SE with several groups, useful for PCA and many pairs of t-tests
 set.seed(123)
@@ -19,14 +20,16 @@ assay(recapSE1)[, 19] <- assay(recapSE1)[, 1] * 0.7
 save(recapSE1, file = "../../data/recapSE1.rda")
 
 ## SE to identify an outlier and try t-test with/without outlier
+
 set.seed(1234)
 recapSE2 <- simulate_proteomics_data(
-           n = 1550,
-           p = 5,
-           prop_outliers = 0.1,
-           group = c("CTRL", "COND"),
-           log_transform = FALSE,
+    n = 1550,
+    p = 5,
+    prop_outliers = 0.1,
+    group = c("CTRL", "COND"),
+    log_transform = FALSE,
     impute = "knn")
+
 ## Remove the 1 value < 0
 assay(recapSE2) <- abs(assay(recapSE2))
 save(recapSE2, file = "../../data/recapSE2.rda")
